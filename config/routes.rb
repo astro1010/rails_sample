52c41145a8a06f9e620f7e4aeb3fa27e81930a86
astro1010 <manote.bbu@gmail.com>
2018-12-22 15:57:26 +0900

    finished chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
 
   get    '/help', to: 'static_pages#help'
@@ -7,5 +6,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get  '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
